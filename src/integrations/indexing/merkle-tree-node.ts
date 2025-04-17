41e7b3307ae2edcdcd8037eefdcb4b26a9e6e712
Georgiy Tarasov <gtarasov.work@gmail.com>
2025-04-17 12:19:40 +0200

    fix: exclude binary

--- src/integrations/indexing/merkle-tree-node.ts
+++ src/integrations/indexing/merkle-tree-node.ts
@@ -2,28 +2,38 @@ import { fileURLToPath } from 'node:url'
 import * as fs from 'node:fs/promises'
 import { createHash } from 'crypto'
 import { TreeNode, TreeNodeType } from './types'
+import { isBinaryFile } from 'isbinaryfile'
 
 export class MerkleTreeNode {
     private calculatedHash: string | null = null
-    public children: MerkleTreeNode[] = []
+
+    /**
+     * Flag indicating that the node is excluded from the tree.
+     * This typically happens when the node is a binary file.
+     */
+    public excluded: boolean = false
 
     constructor(
         readonly path: string,
-        readonly type: TreeNodeType
+        readonly type: TreeNodeType,
+        readonly children: MerkleTreeNode[] = []
     ) {}
 
     async buildHashes() {
         // Leaf node. Calculate hash immediately.
         if (this.type === 'file') {
-            this.calculatedHash = await this.generateFileHash()
+            const hashCandidate = await this.generateFileHash()
+            if (hashCandidate === null) {
+                this.excluded = true
+            } else {
+                this.calculatedHash = hashCandidate
+            }
             return this
         }
 
         // First calculate hashes for all child directories recursively
         for (const child of this.children) {
-            if (child.type === 'dir') {
-                await child.buildHashes()
-            }
+            await child.buildHashes()
         }
 
         // Now compute hash based on children's hashes and names
@@ -33,6 +43,11 @@ export class MerkleTreeNode {
         this.children.sort((a, b) => a.path.localeCompare(b.path))
 
         for (const child of this.children) {
+            // Skip if the child is a binary
+            if (child.excluded) {
+                continue
+            }
+
             // Combine path and hash to create the directory hash
             hasher.update(child.path)
             hasher.update(child.hash)
@@ -49,9 +64,17 @@ export class MerkleTreeNode {
         return this.calculatedHash
     }
 
+    /**
+     * Traverse the tree and convert it to the `TreeNode` objects.
+     * @param parentId - The parent node id.
+     * @returns A generator of tree nodes.
+     */
     *toTreeNodes(parentId?: string): Generator<TreeNode> {
         yield this.toTreeNode(parentId)
         for (const child of this.children) {
+            if (child.excluded) {
+                continue
+            }
             yield* child.toTreeNodes(this.hash)
         }
     }
@@ -62,14 +85,21 @@ export class MerkleTreeNode {
      */
     toLeafNodesMap(): Map<string, MerkleTreeNode> {
         const map = new Map<string, MerkleTreeNode>()
+
         function dfs(node: MerkleTreeNode) {
-            if (node.type === 'file') {
+            if (node.type === 'file' && !node.excluded) {
                 map.set(node.hash, node)
             }
+
             for (const child of node.children) {
+                if (child.excluded) {
+                    continue
+                }
+
                 dfs(child)
             }
         }
+
         dfs(this)
         return map
     }
@@ -84,7 +114,15 @@ export class MerkleTreeNode {
 
     private async generateFileHash() {
         const filePath = fileURLToPath(this.path)
-        const fileContent = await fs.readFile(filePath)
-        return createHash('sha256').update(new Uint8Array(fileContent)).digest('hex')
+        try {
+            const fileContent = await fs.readFile(filePath)
+            const isBinary = await isBinaryFile(filePath)
+            if (isBinary) {
+                return null
+            }
+            return createHash('sha256').update(new Uint8Array(fileContent)).digest('hex')
+        } catch (e) {
+            return null
+        }
     }
 }
